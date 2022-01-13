5c19489d091ac75aeba5ae6d1a7cef023471866c
hithendhra <82403816+hithendhra@users.noreply.github.com>
2022-01-13 20:47:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,11 @@
-noseX=0;
-noseY=0;
-
-function preload() 
-{
-    clown_nose=loadImage('https://i.postimg.cc/pd7NY46g/Clown1.png');
-}
-
 function setup()
 {
-    canvas=createCanvas(300,300);
+    canvas=createCanvas(280,280);
     canvas.center();
-    video=createCapture(VIDEO);
-    video.size(300,300);
-    video.hide();
+    background("reds");
 
-    poseNet=ml5.poseNet(video,modelloaded);
-    poseNet.on('pose',gotposes);
 }
-
-function draw()
+function ClearCanvas()
 {
-    image(video,0,0,300,300);
-    image(clown_nose,noseX-25,noseY-15,50,50);
-}
-
-function take_snapshot()
-{
-    save('my_filter_image.png');
-}
-
-
-function modelloaded()
-{
-    console.log('Model Loaded');
-}
-
-function gotposes(results)
-{
-  if (results.length > 0)
-{
-      console.log(results);
-  noseX=results[0].pose.nose.x;
-  noseY=results[0].pose.nose.y;
-
-      console.log("NOSE X = "+noseX);
-      console.log("NOSE Y = "+noseY);
-}
+    background("white");
 }
